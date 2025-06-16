d1ff00694665613febc5dd9daad88cd2a5e81aab
2827700630 <2827700630@qq.com>
2025-06-17 02:14:17 +0800

    烧录测试完成
    经过多次测试新更新已经确保稳定

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -1,46 +1,12 @@
-# 2025-06-16T22:31:37.014270400
+# 2025-06-17T02:00:52.705882300
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="Zynq7010Oscilloscope2")
 
 platform = client.get_component(name="platform")
-status = platform.update_hw(hw_design = "$COMPONENT_LOCATION/../design_1_wrapper.xsa")
-
 status = platform.build()
 
 comp = client.get_component(name="hello_world")
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
 comp.build()
 
